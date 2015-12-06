17b07e7033537ead6121bd1aa00271fe75b36b29
Yanchevskaya Anna <yanchevskayaa@gmail.com>
2015-12-06 16:53:50 +0200

    binary search

---
+++ YanchevskayaAnna/src/week1_home/BinarySearchAlgorithm/OrderedApp.java
@@ -0,0 +1,40 @@
+package week1_home.BinarySearchAlgorithm;
+
+public class OrderedApp {
+    public static void main(String[] args) {
+
+        int maxSize = 100;
+        OrdArray arr;
+        arr = new OrdArray(maxSize);
+
+        arr.insert(77);
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(11);
+        arr.insert(66);
+        arr.insert(33);
+
+        System.out.println("Test display()");
+        arr.display();
+
+        System.out.println("Test find (55), search key = 55 ");
+        int searchKey = 55;
+        if (arr.find(searchKey) != arr.size())
+            System.out.println("Found " + searchKey);
+        else
+            System.out.println("Can't find " + searchKey);
+
+        arr.delete(55);
+        arr.delete(99);
+
+        System.out.println("Test display() after delete 55 & 99");
+
+        arr.display();
+
+    }
+}
+
