805062b763eaf500fd51c610c08875b843a73eeb
Dima Virtuemaster <dima.kiev@gmail.com>
2015-12-07 21:06:09 +0200

    HomeWork0 CARD_ACCESS_SYSTEM Ready)))))))))))))

---
+++ DimaShkurupiy/src/week_01_home/card_code/StartCardCode.java
@@ -0,0 +1,13 @@
+package week_01_home.card_code;
+
+import week_01_home.card_code.controller.AppController;
+
+/**
+ * Created by Dima on 07.12.2015.
+ */
+public class StartCardCode {
+
+    public static void main(String[] args) {
+        new AppController();
+    }
+}
