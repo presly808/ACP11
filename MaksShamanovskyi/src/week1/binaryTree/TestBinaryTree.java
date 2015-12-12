65e142ca703b9dbe8d92a3a8ef3256c120a9d221
presly808 <presly808@gmail.com>
2015-12-12 16:30:13 +0200

    max refactored map

--- MaksShamanovskyi/src/week1/binaryTree/TestBinaryTree.java
+++ MaksShamanovskyi/src/week1/binaryTree/TestBinaryTree.java
@@ -1,10 +1,12 @@
 package week1.binaryTree;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class TestBinaryTree {
     public static void main(String[] args) {
-        BinaryTree<Integer> tree = new BinaryTree<Integer>();
+        IBinaryTree<Integer> tree = new BinaryTree<>();
         tree.add(20);
         tree.add(30);
         tree.add(15);
@@ -15,17 +17,18 @@ public class TestBinaryTree {
         tree.add(8);
         tree.add(13);
 
-        Iterator iter = tree.getIterator();
-        Object[] mas = new Object[9];
-        mas[0] = iter.next();
-        mas[1] = iter.next();
-        mas[2] = iter.next();
-        mas[3] = iter.next();
-        mas[4] = iter.next();
-        mas[5] = iter.next();
-        mas[6] = iter.next();
-        mas[7] = iter.next();
-        mas[8] = iter.next();
-        System.out.println(iter.hasNext());
+        Iterator<Integer> iter = tree.iterator();
+
+        //Object[] mas = new Object[9];
+
+        List<Integer> arr = new ArrayList<>();
+        while(iter.hasNext()){
+            arr.add(iter.next());
+        }
+
+
+        for (Integer integer : arr) {
+            System.out.println(integer);
+        }
     }
 }
