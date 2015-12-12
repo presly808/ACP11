b190394fc30e0bdaf0ee900e3ed49cff3be1733b
1 <starlight123@mail.ru>
2015-12-12 11:18:45 +0200

    Tourniquet

--- ValeriyHolovko/src/tourniquet/controller/iTourniquetController.java
+++ ValeriyHolovko/src/tourniquet/controller/iTourniquetController.java
@@ -7,14 +7,38 @@ import tourniquet.model.Card;
  */
 public interface iTourniquetController {
 
-    boolean registerNew(Integer cardNumber, String ownerName);
-    boolean scanIn(Card card);
-    boolean scanOut(Card card);
-    void showAllCards();
-    void showCurrentIns();
-    void showAllIns(String date);
+    Card registerNew(Integer cardNumber, String ownerName) throws CardAlreadyExistsException;
+    void scanIn(Card card) throws CardIsAlreadyInException;
+    void scanOut(Card card) throws CardIsNotInException;
+    String showAllCards() throws NoCardsException;
+    String showCurrentIns();
+    String showPassTime(Card card, int day, int month);
+    String showHistoryOfPass(Card card);
+    String showWorkingTime (Card card, int month, int day);
+    String showWorkingTime (Card card, int month);
 
 
+    class CardAlreadyExistsException extends Exception {
+        public CardAlreadyExistsException(String message) {
+            super(message);
+        }
+    }
 
+    class CardIsAlreadyInException extends Exception {
+        public CardIsAlreadyInException(String message) {
+            super(message);
+        }
+    }
 
+    class CardIsNotInException extends Exception {
+        public CardIsNotInException(String message) {
+            super(message);
+        }
+    }
+
+    class NoCardsException extends Exception {
+        public NoCardsException(String message) {
+            super(message);
+        }
+    }
 }
