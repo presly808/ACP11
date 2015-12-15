0cd00167b1ec2d19262c158aa59f682527c9cbee
shamanovkiy <some4day@gmail.com>
2015-12-15 19:25:37 +0200

    modified testBinaryTree

--- MaksShamanovskyi/src/week1/binaryTree/TestBinaryTree.java
+++ MaksShamanovskyi/src/week1/binaryTree/TestBinaryTree.java
@@ -1,5 +1,7 @@
 package week1.binaryTree;
 
+import java.util.Iterator;
+
 public class TestBinaryTree {
     public static void main(String[] args) {
         BinaryTree<Integer> tree = new BinaryTree<Integer>();
@@ -12,6 +14,18 @@ public class TestBinaryTree {
         tree.add(7);
         tree.add(8);
         tree.add(13);
-        
+
+        Iterator iter = tree.getIterator();
+        Object[] mas = new Object[9];
+        mas[0] = iter.next();
+        mas[1] = iter.next();
+        mas[2] = iter.next();
+        mas[3] = iter.next();
+        mas[4] = iter.next();
+        mas[5] = iter.next();
+        mas[6] = iter.next();
+        mas[7] = iter.next();
+        mas[8] = iter.next();
+        System.out.println(iter.hasNext());
     }
 }
