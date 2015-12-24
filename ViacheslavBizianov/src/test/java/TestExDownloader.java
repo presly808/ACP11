28be81f2bdfbe666a18672850e5b540b491dad70
bizianov <viacheslav_bizianov@ukr.net>
2015-12-24 14:42:40 +0200

    ExDownloader TDD

--- ViacheslavBizianov/src/test/java/TestExDownloader.java
+++ ViacheslavBizianov/src/test/java/TestExDownloader.java
@@ -1,4 +1,6 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import jsoup.ExDownloader;
 
@@ -11,6 +13,14 @@ public class TestExDownloader {
 
     private ExDownloader exDownloader = new ExDownloader();
     private String PATH = "http://www.ex.ua/78657081?r=82416,80928";
+
+    @Before
+    public void deleteFile(){
+        File file = new File("C:\\Users\\slava23\\ACP11\\ViacheslavBizianov\\src\\main\\resources\\1.jpg");
+        if (file.exists()){
+            file.delete();
+        }
+    }
 
     @Test
     public void downLoadAllFiles(){
