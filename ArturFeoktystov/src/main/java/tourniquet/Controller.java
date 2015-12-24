bc8b835ad15e924a022ca5dddc304c79525a6079
ArturFeoktystov <arthur29@ukr.net>
2015-12-24 15:19:34 +0200

    XML (Create / Read file)

--- ArturFeoktystov/src/main/java/tourniquet/Controller.java
+++ ArturFeoktystov/src/main/java/tourniquet/Controller.java
@@ -23,67 +23,67 @@ public class Controller {
     }
 
     //-----------------------------------------------------
-    public Card addCard(Card card) {
-        if (database.getCards().size() > card.getCardID()) System.out.println("Card with this ID already exist");
-        //throw new RuntimeException("Card with this ID already exist");
-        database.getCards().add(card);
-        database.getTime().add(new ArrayList<Time>());
-        return card;
-    }
+//    public Card addCard(Card card) {
+//        if (database.getCards().size() > card.getCardID()) System.out.println("Card with this ID already exist");
+//        //throw new RuntimeException("Card with this ID already exist");
+//        database.getCards().add(card);
+//        database.getTime().add(new ArrayList<Time>());
+//        return card;
+//    }
 
 
     //-------------------------------- Scan ---------------------
-    public void scanIn(Card card) {
-        ArrayList<Time> times = database.getTime().get(card.getCardID());
-        Time lastTime = times.get(times.size() - 1);
-
-        //work with Calendar
-        Calendar calendar = new GregorianCalendar();
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-
-        if (isTheSameDay(card, month, day)) {
-            if (lastTime.getEntrance() != 0) {
-                throw new RuntimeException("This card has already entered");
-            } else {
-                Date date = new Date();
-                lastTime.setEntrance(date.getTime());
-            }
-        } else {
-            Time newTimeTracking = new Time(day, month, 0, 0);
-            times.add(newTimeTracking);
-            Date date = new Date();
-            newTimeTracking.setEntrance(date.getTime());
-        }
-    }
+//    public void scanIn(Card card) {
+//        ArrayList<Time> times = database.getTime().get(card.getCardID());
+//        Time lastTime = times.get(times.size() - 1);
+//
+//        //work with Calendar
+//        Calendar calendar = new GregorianCalendar();
+//        int month = calendar.get(Calendar.MONTH);
+//        int day = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//
+//        if (isTheSameDay(card, month, day)) {
+//            if (lastTime.getEntrance() != 0) {
+//                throw new RuntimeException("This card has already entered");
+//            } else {
+//                Date date = new Date();
+//                lastTime.setEntrance(date.getTime());
+//            }
+//        } else {
+//            Time newTimeTracking = new Time(day, month, 0, 0);
+//            times.add(newTimeTracking);
+//            Date date = new Date();
+//            newTimeTracking.setEntrance(date.getTime());
+//        }
+//    }
 
     //-----------------------------------------------------
-    public void scanOut(Card card) {
-
-        ArrayList<Time> times = database.getTime().get(card.getCardID());
-        Time lastTime = times.get(times.size()-1);
-
-        Calendar calendar = new GregorianCalendar();
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        if (lastTime.getEntrance() == 0){
-            throw new RuntimeException("This card hasn't entered yet");
-        } else {
-            Date date = new Date();
-            lastTime.setTotalTime(lastTime.getTotalTime()+(date.getTime() - lastTime.getEntrance()));
-            lastTime.setEntrance(0);
-        }
-
-    }
+//    public void scanOut(Card card) {
+//
+//        ArrayList<Time> times = database.getTime().get(card.getCardID());
+//        Time lastTime = times.get(times.size()-1);
+//
+//        Calendar calendar = new GregorianCalendar();
+//        int month = calendar.get(Calendar.MONTH);
+//        int day = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//        if (lastTime.getEntrance() == 0){
+//            throw new RuntimeException("This card hasn't entered yet");
+//        } else {
+//            Date date = new Date();
+//            lastTime.setTotalTime(lastTime.getTotalTime()+(date.getTime() - lastTime.getEntrance()));
+//            lastTime.setEntrance(0);
+//        }
+//
+//    }
 
 
     //-------------------------------- Show ---------------------
-    public void showAllCards() {
-        for (Card card : database.getCards())
-            System.out.println(card.getCardID() + " - " + card.getCardHolder());
-    }
+//    public void showAllCards() {
+//        for (Card card : database.getCards())
+//            System.out.println(card.getCardID() + " - " + card.getCardHolder());
+//    }
 
     //-----------------------------------------------------
     public void showBestWorkers() {
@@ -96,18 +96,17 @@ public class Controller {
     }
 
 
-    private boolean isTheSameDay(Card card, int month, int day) {
-        ArrayList<Time> times = database.getTime().get(card.getCardID());
-        Time lastTime = times.get(times.size() - 1);
+//    private boolean isTheSameDay(Card card, int month, int day) {
+//        ArrayList<Time> times = database.getTime().get(card.getCardID());
+//        Time lastTime = times.get(times.size() - 1);
+//
+//        if (month > lastTime.getMonth()) {
+//            return false;
+//        } else {
+//            if (day > lastTime.getDay()) {
+//                return false;
+//            }
+//        }
+//        return true;
 
-        if (month > lastTime.getMonth()) {
-            return false;
-        } else {
-            if (day > lastTime.getDay()) {
-                return false;
-            }
-        }
-        return true;
-
-    }
 }
