877b0ff833a537b9a2fe6e8453768b458bd16138
ArturFeoktystov <arthur29@ukr.net>
2016-01-10 00:29:59 +0200

    week 4

--- ArturFeoktystov/src/main/java/Chat/ChatClient.java
+++ ArturFeoktystov/src/main/java/Chat/ChatClient.java
@@ -15,22 +15,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-/**
- * A simple Swing-based client for the chat server.  Graphically
- * it is a frame with a text field for entering messages and a
- * textarea to see the whole dialog.
- *
- * The client follows the Chat Protocol which is as follows.
- * When the server sends "SUBMITNAME" the client replies with the
- * desired screen name.  The server will keep sending "SUBMITNAME"
- * requests as long as the client submits screen names that are
- * already in use.  When the server sends a line beginning
- * with "NAMEACCEPTED" the client is now allowed to start
- * sending the server arbitrary strings to be broadcast to all
- * chatters connected to the server.  When the server sends a
- * line beginning with "MESSAGE " then all characters following
- * this string should be displayed in its message area.
- */
+
+
 public class ChatClient {
 
     BufferedReader in;
@@ -39,14 +25,8 @@ public class ChatClient {
     JTextField textField = new JTextField(40);
     JTextArea messageArea = new JTextArea(8, 40);
 
-    /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Return in the
-     * listener sends the textfield contents to the server.  Note
-     * however that the textfield is initially NOT editable, and
-     * only becomes editable AFTER the client receives the NAMEACCEPTED
-     * message from the server.
-     */
+
+
     public ChatClient() {
 
         // Layout GUI
@@ -58,11 +38,9 @@ public class ChatClient {
 
         // Add Listeners
         textField.addActionListener(new ActionListener() {
-            /**
-             * Responds to pressing the enter key in the textfield by sending
-             * the contents of the text field to the server.    Then clear
-             * the text area in preparation for the next message.
-             */
+
+             //Press Enter -> sending the contents of the text field to the server.
+             //Then clear text area
             public void actionPerformed(ActionEvent e) {
                 out.println(textField.getText());
                 textField.setText("");
@@ -70,9 +48,7 @@ public class ChatClient {
         });
     }
 
-    /**
-     * Prompt for and return the address of the server.
-     */
+    //ask and return server IP
     private String getServerAddress() {
         return JOptionPane.showInputDialog(
                 frame,
@@ -81,9 +57,7 @@ public class ChatClient {
                 JOptionPane.QUESTION_MESSAGE);
     }
 
-    /**
-     * Prompt for and return the desired screen name.
-     */
+    //ask and return name
     private String getName() {
         return JOptionPane.showInputDialog(
                 frame,
@@ -92,9 +66,9 @@ public class ChatClient {
                 JOptionPane.PLAIN_MESSAGE);
     }
 
-    /**
-     * Connects to the server then enters the processing loop.
-     */
+
+     //Connects to the server
+     // enters the processing loop.
     private void run() throws IOException {
 
         // Make connection and initialize streams
@@ -117,9 +91,8 @@ public class ChatClient {
         }
     }
 
-    /**
-     * Runs the client as an application with a closeable frame.
-     */
+
+    // Runs the client as an application with a closeable frame.
     public static void main(String[] args) throws Exception {
         ChatClient client = new ChatClient();
         client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
