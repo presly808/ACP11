fd4cfba8daae1811fe584a4a7e8e0834c6d2370f
shamanovskiy <some4day@gmail.com>
2016-01-15 16:14:34 +0200

    multythreads ex

--- MaksShamanovskyi/src/main/java/week5_Max/ex_downloader/TestMultyDownloader.java
+++ MaksShamanovskyi/src/main/java/week5_Max/ex_downloader/TestMultyDownloader.java
@@ -3,6 +3,6 @@ package week5_Max.ex_downloader;
 public class TestMultyDownloader {
 
     public static void main(String[] args) {
-        new SearcherDownloader("http://www.ex.ua/search?s=rammstein+mutter", "TEST").run();
+        new MultyDownloader("выживший 2015 фильм дикаприо", "C:\\Users\\Макс\\TEST").start();
     }
 }
