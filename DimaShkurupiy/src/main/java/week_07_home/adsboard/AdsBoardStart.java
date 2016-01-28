7768ac7636d25a01f07640ea9a81cc8cd085ff21
Dima Virtuemaster <dima.kiev@gmail.com>
2016-01-28 09:39:24 +0200

    move files to my 2nd comp

---
+++ DimaShkurupiy/src/main/java/week_07_home/adsboard/AdsBoardStart.java
@@ -0,0 +1,10 @@
+package week_07_home.adsboard;
+
+/**
+ * Created by Dima on 27.01.2016.
+ */
+public class AdsBoardStart {
+    public static void main(String[] args) {
+        System.out.println("Started");
+    }
+}
---
+++ DimaShkurupiy/src/main/java/week_07_home/adsboard/model/Ads.java
@@ -0,0 +1,20 @@
+package week_07_home.adsboard.model;
+
+/**
+ * Created by Dima on 27.01.2016.
+ */
+public class Ads {
+    private String owner; // private_person company non-commercial gov_office etc
+    private String[] adsType; // service info rules admin meeting buy sell change gift greetings necrolog proposal hireing ask search press-release offerta etc
+    private String[] list; //
+    private String country;
+    private String[] region;
+    private String city;
+    private String[] medias;
+    private String[] links;
+    private String[] related;
+    private String[] backLinks;
+    private String adsBody;
+    private String customFields; // for each type and list and product variation
+
+}
